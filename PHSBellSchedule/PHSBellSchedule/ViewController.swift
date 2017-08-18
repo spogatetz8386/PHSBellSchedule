fb09e3ae8a31f502a22b6d3bf6dea13ff363afc0
Scott Pogatetz <apcs2@p-162mba-29.d214.org>
2017-08-18 09:43:27 -0500

    Added dropdown

--- PHSBellSchedule/PHSBellSchedule/ViewController.swift
+++ PHSBellSchedule/PHSBellSchedule/ViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import BTNavigationDropdownMenu
 
 class ViewController: UIViewController {
-
+    var menu : BTNavigationDropdownMenu!
     override func viewDidLoad() {
+        let items = ["Regular", "PEP1", "PEP2", "Locker Cleanup", "Finals Day 1", "Finals Day 2", "Finals Day 3", "Knights Way", "Early Dismissal", "Special Assembly"]
         super.viewDidLoad()
+        menu = BTNavigationDropdownMenu(navigationController: self.navigationController, containerView: self.navigationController!.view, title: "Schedule", items: items as [AnyObject])
+        menu.menuTitleColor = UIColor.blue
+        menu.backgroundColor = UIColor.cyan
+        self.navigationItem.titleView = menu
+
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
