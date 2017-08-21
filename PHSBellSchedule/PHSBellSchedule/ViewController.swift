b8269d621d198e9486538142ddeb567822f92202
apcs2 <acimack9005@stu.d214.org>
2017-08-21 09:29:11 -0500

    dates

--- PHSBellSchedule/PHSBellSchedule/ViewController.swift
+++ PHSBellSchedule/PHSBellSchedule/ViewController.swift
@@ -11,6 +11,21 @@ import BTNavigationDropdownMenu
 
 class ViewController: UIViewController {
     var menu : BTNavigationDropdownMenu!
+    
+    let date = Date()
+    let formatter = DateFormatter()
+    
+    let test = ["08/21/2017"]
+    let pepAsm1 = ["08/18/2017", "12/08/2017", "04/06/2018"]
+    let pepAsm2 = ["09/15/2017"]
+    let kwDay = ["09/28/2017", "11/02/2017", "01/25/2018", "03/08/2018", "04/19/2018"]
+    let earlyDis = ["03/23/2018", "05/25/2018"]
+    let specialAsm = ["05/23/2018"]
+    let lockerCU = "05/25/2018"
+    let final1 = ["12/19/2017", "05/30/2018"]
+    let final2 = ["12/20/2017", "05/31/2018"]
+    let final3 = ["12/21/2017", "06/01/2018"]
+    
     override func viewDidLoad() {
         let items = ["Regular", "PEP1", "PEP2", "Locker Cleanup", "Finals Day 1", "Finals Day 2", "Finals Day 3", "Knights Way", "Early Dismissal", "Special Assembly"]
         super.viewDidLoad()
@@ -21,6 +36,19 @@ class ViewController: UIViewController {
 
 
         // Do any additional setup after loading the view, typically from a nib.
+        
+        formatter.dateFormat = "MM/dd/yyyy"
+        let currentDate = formatter.string(from: date)
+        print(currentDate)
+        
+        if test.contains("\(currentDate)")
+        {
+            print("yes")
+        }
+        
+        
+        
+
     }
 
     override func didReceiveMemoryWarning() {
