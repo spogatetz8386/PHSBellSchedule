3b4f1605d1454e24bf42aa1cf6718d9c89f9d1a9
Scott Pogatetz <apcs2@p-162mba-29.d214.org>
2017-08-21 09:55:54 -0500

    stuff

--- PHSBellSchedule/PHSBellSchedule/ViewController.swift
+++ PHSBellSchedule/PHSBellSchedule/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     let final2 = ["12/20/2017", "05/31/2018"]
     let final3 = ["12/21/2017", "06/01/2018"]
     
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         let items = ["Regular", "PEP1", "PEP2", "Locker Cleanup", "Finals Day 1", "Finals Day 2", "Finals Day 3", "Knights Way", "Early Dismissal", "Special Assembly"]
         super.viewDidLoad()
@@ -49,7 +50,34 @@ class ViewController: UIViewController {
         menu.didSelectItemAtIndexHandler = {[weak self] (indexPath: Int) -> () in
             print("Did select item at index: \(indexPath)")
             if(indexPath == 0){
-                
+                self?.imageView.image = UIImage(named: "regular.png")
+            }
+            else if(indexPath == 1){
+                self?.imageView.image = UIImage(named: "pepOne.png")
+            }
+            else if(indexPath == 2){
+                self?.imageView.image = UIImage(named: "pepTwo.png")
+            }
+            else if(indexPath == 3){
+                self?.imageView.image = UIImage(named: "locker.png")
+            }
+            else if(indexPath == 4){
+                self?.imageView.image = UIImage(named: "finalsOne.png")
+            }
+            else if(indexPath == 5){
+                self?.imageView.image = UIImage(named: "finalsTwo.png")
+            }
+            else if(indexPath == 6){
+                self?.imageView.image = UIImage(named: "finalsThree.png")
+            }
+            else if(indexPath == 7){
+                self?.imageView.image = UIImage(named: "knightsWay.png")
+            }
+            else if(indexPath == 8){
+                self?.imageView.image = UIImage(named: "early.png")
+            }
+            else if(indexPath == 9){
+                self?.imageView.image = UIImage(named: "special.png")
             }
         }
     }
